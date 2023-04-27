bf5a2af3f167ff3da9d65a6747cad9754dbbc954
Mikeal <mwiemszra@gmail.com>
2023-04-27 23:49:13 +0200

    🚚 private and public folder

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/style.module.css'
+import styles from '../private/css/style.module.css'
 
 export default function Home() {
   return (
